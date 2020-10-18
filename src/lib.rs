f24975ab52b17b6789007ba37bc32c8de7744087
Nils Martel <akantorabc@yahoo.de>
2020-10-18 21:43:15 +0200

    Start coding

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,27 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+// TODO make generic
+pub struct Graph {
+    // might fail, if the parent of a nodes are greater than the nodes index
+    nodes: Vec<Node>,
+}
+
+pub struct Node {
+    parent: Vec<usize>,
+}
+
+struct PositionedNode {
+    pub field: Node,
+    pub value: usize,
+}
+
+impl Graph {
+    fn position(self) -> Vec<PositionedNode> {
+        let mut Graph {nodes} = self;
+
+        for (i, node) in nodes.iter().enumerate() {
+        }
+    }
+}
